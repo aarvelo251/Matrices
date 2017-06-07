e4fb99f7c522c33ab2b1bb31f87c96814eb19fe6
aarvelo251 <aarvelo251@gmail.com>
2017-06-07 20:56:41 -0300

    Matrices
    Matrices

--- src/Repaso/Bidimensional.java
+++ src/Repaso/Bidimensional.java
@@ -1,23 +1,57 @@
 package Repaso;
 
 public class Bidimensional {
+	
 	public static void main(String[] args) {
 		
-		int [][] ejemplo = new int [2][3];
+		int[][] ejemplo = new int[2][3];
+		
+		System.out.println( "Se llena el arreglo de manera tradicional" );
 		
 		/**
 		 * Este bloque de código es para llenar el arreglo
 		 */
-		
-		System.out.println(ejemplo.length);
-		
-		for (int i =0; i < 2; i++ ){
-		
-		for (int j = 0; j < 3; j++){
+		for (int i = 0; i < 2; i++) {
 			
-			ejemplo[i][j] = (i * j);
+			for (int j = 0; j < 3; j++) {
+				
+				ejemplo[i][j] = ( i * j );
+				
+			}
+			
 		}
+		
+		System.out.println( "Se imprime el arreglo de manera tradicional" );
+		
+		
+		/**
+		 * Este bloque de código e para imprimir el arreglo
+		 */
+		for (int i = 0; i < 2; i++) {
+			
+			for (int j = 0; j < 3; j++) {
+				
+				System.out.println( ejemplo[i][j] + "Este es el valor contenido en la posicion " + i + " --- " + j);
+				
+			}
 		}
+		
+		System.out.println( "Se imprime el arreglo utilizar 'FOREACH'" );
+		
+		//FOREACH
+		for( int[] x : ejemplo ){
+			
+			for( int y : x ){
+
+				System.out.println( y );
+				
+			}
+		}
+		
+	
+
+
+		
 		/**
 		 * Este bloque de código es para imprimir el arreglo
 		 */
